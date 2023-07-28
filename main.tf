47b4dd855f24e0b8c4a297127a9cfc0b5a843b3b
Khushal Sarode <105520366+Khushalsarode@users.noreply.github.com>
2023-07-29 02:58:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "Prod-Server" {
  ami = "ami-053b0d53c279acc90"
  instance_type = "t2.micro"
  availability_zone = "us-east-1a"
- key_name = "rohan"
+ key_name = "test"
  network_interface {
  device_index = 0
  network_interface_id = aws_network_interface.proj-ni.id
@@ -139,7 +139,7 @@ resource "aws_instance" "Prod-Server" {
      sudo apt-get update -y
      sudo apt install docker.io -y
      sudo systemctl enable docker
-     sudo docker run -itd -p 8086:8081 rohanraparthi/banking:1.0
+     sudo docker run -itd -p 8084:8081 khushalsarode/financeme:1.0
      sudo docker start $(docker ps -aq)
      sudo docker run -itd -p 9100:9100 prom/node-exporter
  EOF
